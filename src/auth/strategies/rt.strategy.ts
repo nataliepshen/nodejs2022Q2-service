aa287828234511de45401a7d7274df3c7e4f5929
nataliepshen <romashki23@mail.ru>
2022-08-07 12:47:22 +0300

    feat: add strategies

---
+++ src/auth/strategies/rt.strategy.ts
@@ -0,0 +1,18 @@
+import { Injectable } from "@nestjs/common";
+import { PassportStrategy } from "@nestjs/passport";
+import { ExtractJwt, Strategy } from "passport-jwt";
+import { JwtPayload } from "../types/JwtPayload.type";
+
+@Injectable()
+export class RtStrategy extends PassportStrategy(Strategy, 'jwt-refresh') {
+    constructor() {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: process.env.JWT_SECRET_REFRESH_KEY,
+            passReqToCallback: true,
+        });
+    }
+    validate(payload: JwtPayload) {
+        return payload;
+    }
+}
